3131f57483e8d011359f3e9ace0136a5fccfda1d
essien emmanuel <essienemma300@gmail.com>
2024-05-24 23:12:04 +0100

    variables

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-echo "Essien Emmanuel PHP starter";
+$title = "Learn PHP From Scratch";
+$heading = "Welcome to the course";
+$body = "In this course, you will learn the fundamentals of the PHP language"
 ?>
 
 <!DOCTYPE html>
@@ -9,21 +11,19 @@ echo "Essien Emmanuel PHP starter";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
-  <title><?php echo 'Learn PHP From Scratch'; ?></title>
+  <title><?= $title ?></title>
 </head>
 
 <body class="bg-gray-100">
   <header class="bg-blue-500 text-white p-4">
     <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch' ?></h1>
+      <h1 class="text-3xl font-semibold"><?= $title ?></h1>
     </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6">
-      <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome to the course' ?></h2>
-      <?= '<p>In this course, you will learn the fundamentals of the PHP language</p>' ?>
-      <?= "this is just a paragraph</p>" ?>
-      <p><?php echo "This is but another paragraph" ?></p>
+      <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
+      <p><?= $body ?></p>
     </div>
   </div>
 </body>
