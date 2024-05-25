88f488d88b4ca5cd89e829f227c40cfaae8d5e3e
essien emmanuel <essienemma300@gmail.com>
2024-05-25 16:01:54 +0100

    variable challenge

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
 <?php
-$title = "Learn PHP From Scratch";
-$heading = "Welcome to the course";
-$body = "In this course, you will learn the fundamentals of the PHP language"
+$pageTitle = "Introduction to PHP";
+$title = "Brad's PHP Blog | $pageTitle";
+$author = "By: John Doe";
+$body = "PHP (Hypertext Preprocessor) is a widely used server-side scripting
+language that has revolutionized web development. With its simplicity,
+flexibility, and vast community support, PHP has become the backbone of
+countless dynamic websites and web applications";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?= $title ?></title>
 </head>
 
-<body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold"><?= $title ?></h1>
-    </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <div class="bg-white rounded-lg shadow-md p-6">
-      <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
-      <p><?= $body ?></p>
-    </div>
-  </div>
+<body>
+  <main>
+    <h1><?= $pageTitle ?></h1>
+    <p><?= $author ?></p>
+    <p>
+      <?= $body ?>
+    </p>
+  </main>
 </body>
 
 </html>
